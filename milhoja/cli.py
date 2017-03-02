6c2a6a24ec678d71babc00d6042ab8e9e863b3f6
Raphael Medaer <raphael@medaer.me>
2017-03-02 16:56:49 +0100

    Prototyped process and CLI commands/arguments.
    The goal of this commit is to define in code what has been discussed
    in https://github.com/audreyr/cookiecutter/issues/784.

--- milhoja/cli.py
+++ milhoja/cli.py
@@ -1,11 +1,57 @@
 # -*- coding: utf-8 -*-
 
 import click
+from cookiecutter.cli import validate_extra_context
+from milhoja import Milhoja # indeed ...
 
+@click.group()
+@click.option(
+    u'-C',
+    default=u'.',
+    help=u'Run as if milhoja was started in <path> instead of the current working directory.',
+    type=click.Path()
+)
+@click.pass_context
+def main(ctx, c):
+    """Script entry point for Milhoja commands.
 
-@click.command()
-def main(args=None):
-    """Console script for milhoja"""
+    Arguments:
+    ctx -- CLI context.
+    c -- Path where to run milhoja
+    """
+    ctx.obj['milhoja'] = Milhoja(c)
 
-if __name__ == "__main__":
-    main()
+@main.command()
+@click.argument('template')
+@click.argument(u'extra_context', nargs=-1, callback=validate_extra_context)
+@click.option(
+    u'-c', u'--checkout',
+    help=u'branch, tag or commit to checkout',
+    default=u'master'
+)
+@click.option(
+    u'--no-input', is_flag=True,
+    help=u'Do not prompt for parameters and only use cookiecutter.json '
+         u'file content',
+)
+@click.pass_context
+def install(ctx, template, **kwargs):
+    ctx.obj['milhoja'].install(
+        template,
+        **kwargs
+    )
+
+@main.command()
+@click.argument(u'extra_context', nargs=-1, callback=validate_extra_context)
+@click.option(
+    u'-c', u'--checkout',
+    help=u'branch, tag or commit to checkout',
+)
+@click.option(
+    u'--no-input', is_flag=True,
+    help=u'Do not prompt for parameters and only use cookiecutter.json '
+         u'file content',
+)
+@click.pass_context
+def upgrade(ctx, **kwargs):
+    ctx.obj['milhoja'].install(**kwargs)
--- milhoja/milhoja.py
+++ milhoja/milhoja.py
@@ -1,1 +1,87 @@
 # -*- coding: utf-8 -*-
+
+
+class Milhoja(object):
+    def __init__(self, path, template_branch='template'):
+        self.path = path
+        self.template_branch = template_branch
+        pass
+
+    def install(self, template, checkout='master', extra_context=None, no_input=False):
+        if extra_context is None:
+            extra_context = {}
+
+        # TODO Assert template branch doesn't exist or raise conflict
+
+        # TODO Create temporary worktree
+
+        # TODO Create orphaned branch 'template' (self.template_branch)
+
+        # TODO Apply cookiecutter
+        # TODO we need to get back cookiecutter context, it needs to patch
+        #      cookiecutter to return created context (a tuple) !
+        #_, context = cookiecutter(
+        #     template, checkout, no_input,
+        #     extra_context=extra_context,
+        #     replay=replay,
+        #     overwrite_if_exists=True,
+        #     output_dir=worktree,
+        #     strip=True
+        # )
+
+        # TODO Commit changes
+
+        # TODO Create Git Note in 'milhoja/template' namespace with template references
+
+        # TODO Create Git Note in 'milhoja/context' namespace with context
+
+        # TODO Attach this notes to last commit
+
+        # TODO Remove worktree (unlink + prune)
+
+        # TODO In self.path: merge template branch into HEAD
+
+        # TODO What to do with conflict ?
+        #        -> Let user resolve ?
+        #        -> Raise an error after analysis ?
+
+        pass
+
+    def upgrade(self, checkout='master', extra_context=None, no_input=False):
+        if extra_context is None:
+            extra_context = {}
+
+        # TODO Assert template branch exist or raise an error
+
+        # TODO Find first template commit in template branch (root).
+        # TODO Try to fetch note from 'milhoja/template' namespace
+
+        # TODO Get last commit in 'template'
+        # TODO Try to fetch note from 'milhoja/context' namespace
+        # TODO Parse context from this note: old_context
+
+        # TODO Merge old_context and extra_context (priority to extra_context)
+
+        # TODO Create temporary EMPTY worktree
+
+        # TODO Move HEAD to template branch WITHOUT checkout
+
+        # TODO Apply cookiecutter with merged context
+        # TODO Such in installation, cookiecutter MUST return a tuple with
+        #      context used
+
+        # TODO Commit changes
+
+        # TODO Create Git Note in 'milhoja/context' namespace with context from cc
+
+        # TODO Attach this notes to last commit
+
+        # TODO Remove worktree (unlink + prune)
+
+        # TODO In self.path: merge template branch into HEAD
+        # NOTE Which kind of merge to do ?
+
+        # TODO Such as installation: what to do with conflict ?
+        #        -> Let user resolve ?
+        #        -> Raise an error after analysis ?
+        pass
